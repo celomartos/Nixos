1449e1b147110c02db50d47625b5c0954a660211
celomartos <marcelo.rosacontreiras@gmail.com>
2026-08-18 21:16:35 -0300

    removed install.sh

--- install.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-sudo nixos-rebuild switch \
-  --flake . \
-  --impure \
-  --option substituters "https://cache.nixos.org/ https://attic.xuyh0120.win/lantian" \
-  --option trusted-public-keys "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY= lantian:EeAUQ+W+6r7EtwnmYjeVwx5kOGEBpjlBfPlzGlTNvHc="
